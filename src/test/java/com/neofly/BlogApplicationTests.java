c42558acfdbead7305bece9d9335f9a01f49bc18
neofly <dooribbs@daum.net>
2020-08-03 09:18:02 +0900

    환경셋팅완료

---
+++ src/test/java/com/neofly/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neofly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
